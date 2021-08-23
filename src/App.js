b84ad212b162e71be773855ab73d6748b6ba685c
danah2319 <danah2139@gmail.com>
2021-08-23 13:39:13 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,100 @@
 import "./App.css";
 import BarChartHomePlanets from "./components/barChartHomePlanets/BarChartHomePlanets";
 import TableVehicleDetails from "./components/tableVehicleDetails/TableVehicleDetails";
+import api from "./api/api";
+
+import { useEffect, useState } from "react";
+
 function App() {
+  const [MaxData, setMaxData] = useState({});
+  const [planets, setPlanets] = useState({});
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get("vehicles");
+      const numOfVehicles = data.count;
+      let maxPopulation = 0;
+      let vehicleData, pilotsData, homeworldData;
+      let i = 1;
+      let count = 0,
+        sum,
+        currentVeicleDetail;
+      while (numOfVehicles > count) {
+        sum = 0;
+        currentVeicleDetail = {};
+        try {
+          vehicleData = await api.get(`vehicles/?page=${i}`);
+        } catch (error) {
+          console.log(error);
+        }
+        count += vehicleData.data.results.length;
+        i++;
+        for (let i = 0; i < vehicleData.data.results.length; i++) {
+          currentVeicleDetail["vehicleName"] = vehicleData.data.results[i].name;
+          let j = 0;
+          while (
+            vehicleData.data.results[i].pilots &&
+            j < vehicleData.data.results[i].pilots.length
+          ) {
+            let pilotLink = vehicleData.data.results[i].pilots[j].replace(
+              "https://swapi.dev/api/",
+              ""
+            );
+            try {
+              pilotsData = await api.get(pilotLink);
+            } catch (error) {
+              console.log(error);
+            }
+
+            if (pilotsData.data) {
+              currentVeicleDetail["pilots"] = currentVeicleDetail["pilots"]
+                ? [...currentVeicleDetail["pilots"], pilotsData.data.name]
+                : [pilotsData.data.name];
+
+              if (pilotsData.data.homeworld) {
+                let homeworldLink = pilotsData.data.homeworld.replace(
+                  "https://swapi.dev/api/",
+                  ""
+                );
+
+                try {
+                  homeworldData = await api.get(homeworldLink);
+                } catch (error) {
+                  console.log(error);
+                }
+                if (homeworldData) {
+                  currentVeicleDetail["planets"] = {
+                    ...currentVeicleDetail["planets"],
+                    [homeworldData.data.name]: parseInt(
+                      homeworldData.data.population
+                    ),
+                  };
+                  //   console.log(homeworldData.data.population);
+                  sum += parseInt(homeworldData.data.population);
+                  setPlanets((prevState) => ({
+                    ...prevState,
+                    [homeworldData.data.name]: parseInt(
+                      homeworldData.data.population
+                    ),
+                  }));
+                }
+              }
+            }
+
+            j++;
+          }
+          if (maxPopulation < sum) {
+            console.log(planets);
+            setMaxData(currentVeicleDetail);
+            maxPopulation = sum;
+          }
+        }
+      }
+    })();
+  }, []);
   return (
     <div>
-      <TableVehicleDetails />
-      <BarChartHomePlanets />
+      <TableVehicleDetails MaxData={MaxData} />
+      <BarChartHomePlanets planets={planets} />
     </div>
   );
 }
--- src/components/tableVehicleDetails/TableVehicleDetails.js
+++ src/components/tableVehicleDetails/TableVehicleDetails.js
@@ -1,85 +1,21 @@
-import { useEffect, useState } from "react";
-import api from "../../api/api";
-
-const TableVehicleDetails = () => {
-  const [MaxData, setMaxData] = useState({});
-  useEffect(() => {
-    (async () => {
-      const { data } = await api.get("vehicles");
-      const count = data.count;
-      let maxPopulation = 0;
-      let vehicleData, pilotsData, homeworldData;
-      let i = 1;
-      while (count > i) {
-        let currentMax = 0;
-        let currentVeicleDetail = {};
-        try {
-          vehicleData = await api.get(`vehicles/${i}`);
-          //   console.log(vehicleData.data);
-        } catch (error) {
-          console.log(error);
-        }
-        if (vehicleData) {
-          currentVeicleDetail = { vehicleName: vehicleData.data.name };
-          let j = 0;
-          while (j < vehicleData.data.pilots.length) {
-            let pilotLink = vehicleData.data.pilots[j].replace(
-              "https://swapi.dev/api/",
-              ""
-            );
-            try {
-              pilotsData = await api.get(pilotLink);
-            } catch (error) {
-              console.log(error);
-            }
-            if (pilotsData.data) {
-              currentVeicleDetail = {
-                ...currentVeicleDetail,
-                pilotName: pilotsData.data.name,
-              };
-              if (pilotsData.data.homeworld) {
-                let homeworldLink = pilotsData.data.homeworld.replace(
-                  "https://swapi.dev/api/",
-                  ""
-                );
-
-                try {
-                  homeworldData = await api.get(homeworldLink);
-                } catch (error) {
-                  console.log(error);
-                }
-                if (homeworldData) {
-                  currentVeicleDetail = {
-                    ...currentVeicleDetail,
-                    homeworldName: homeworldData.data.name,
-                    homeworldPopulatione: parseInt(
-                      homeworldData.data.population
-                    ),
-                  };
-                  //   console.log(homeworldData.data.population);
-                  if (parseInt(homeworldData.data.population) > currentMax) {
-                    currentMax = parseInt(homeworldData.data.population);
-                  }
-                }
-              }
-            }
-
-            j++;
-          }
-        }
-        if (maxPopulation < currentMax) {
-          console.log(currentVeicleDetail);
-          setMaxData(currentVeicleDetail);
-          maxPopulation = currentMax;
-        }
-        i++;
-      }
-    })();
-  }, []);
+const TableVehicleDetails = ({ MaxData }) => {
   const showMaxDetailes = () => {
     const display = [];
-    for (let key in MaxData) {
-      display.push(<td>{MaxData[key]}</td>);
+    if (MaxData) {
+      display.push(<td>{MaxData["vehicleName"]}</td>);
+      if (MaxData["pilots"] && MaxData["pilots"].length > 0) {
+        MaxData["pilots"].forEach((element) => {
+          display.push(<td>{element}</td>);
+        });
+      }
+
+      for (let key in MaxData["planets"]) {
+        display.push(
+          <td>
+            {key} : {MaxData["planets"][key]}
+          </td>
+        );
+      }
     }
     return display;
   };
